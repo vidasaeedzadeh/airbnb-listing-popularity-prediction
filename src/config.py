1ca8013f45ae8b4fa2bbb8274f0d4f55888ac081
Vida Saeedzadeh <vida.saeidzadeh@gmail.com>
2026-03-15 10:48:29 -0400

    Add project configuration with paths and core constants

---
+++ src/config.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+################Projest Root and Data Path###################
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+RAW_DATA_FILE = RAW_DATA_DIR / "AB_NYC_2019.csv"
+CLEANED_DATA_FILE = PROCESSED_DATA_DIR / "airbnb_cleaned.csv"
+
+
+################Results and Figures###################
+
+RESULTS_DIR = PROJECT_ROOT / "results"
+FIGURES_DIR = PROJECT_ROOT / "figures"
+
+TABLES_DIR = RESULTS_DIR / "tables"
+METRICS_DIR = RESULTS_DIR / "metrics"
+MODELS_DIR = RESULTS_DIR / "models"
+
+################Data and Modeling Configuration###################
+
+TARGET_COLUMN = "reviews_per_month"
+
+
